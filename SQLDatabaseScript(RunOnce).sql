043915d261b611572d51d4c769e21c97a796c028
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2024-12-17 23:38:38 +0800

    Update SQLDatabaseScript(RunOnce).sql

--- SQLDatabaseScript(RunOnce).sql
+++ SQLDatabaseScript(RunOnce).sql
@@ -1,7 +1,9 @@
-CREATE DATABASE portal_db;
+
+-- CREATE DATABASE portal_db;
 
 USE portal_db;
 
+/*
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -15,17 +17,18 @@ CREATE TABLE subjects (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     type ENUM('Major', 'Minor') NOT NULL,
-    course VARCHAR(10) NOT NULL
+    course VARCHAR(10) NOT NULL,
+    course_code VARCHAR(50) NOT NULL
 );
 
-INSERT INTO subjects (name, type, course) VALUES
-('CMPSC 116 Database Systems', 'Major', 'BSCS'),
-('CMPSC 131 Data Structures', 'Major', 'BSCS'),
-('SOCSC 03 Understanding The Self', 'Minor', 'BSCS'),
-('HUM 12 Reading Visual Arts', 'Minor', 'BSCS'),
-('IT 16 Database Systems', 'Major', 'BSIT'),
-('SOCSC 03 Understanding The Self', 'Minor', 'BSIT'),
-('HUM 12 Reading Visual Arts', 'Minor', 'BSIT');
+INSERT INTO subjects (name, type, course, course_code) VALUES
+('Database Systems', 'Major', 'BSCS', 'CMPSC 116'),
+('Data Structures', 'Major', 'BSCS', 'CMPSC 131'),
+('Understanding The Self', 'Minor', 'BSCS', 'SOCSC 03'),
+('Reading Visual Arts', 'Minor', 'BSCS', 'HUM 12' ),
+('Database Systems', 'Major', 'BSIT', 'IT 16'),
+('Understanding The Self', 'Minor', 'BSIT', 'SOCSC 03'),
+('Reading Visual Arts', 'Minor', 'BSIT', 'HUM12');
 
 
 CREATE TABLE enrolled_subjects (
@@ -35,3 +38,12 @@ CREATE TABLE enrolled_subjects (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
+*/
+
+/*
+ALTER TABLE users 
+ADD COLUMN address VARCHAR(255),
+ADD COLUMN phone_number VARCHAR(15); -- To store the file path of the uploaded image
+*/
+alter table users modify  profile_picture VARCHAR(255) DEFAULT 'public/uploads/default-profile.png';
+alter table users modify  profile_picture VARCHAR(255) DEFAULT '/uploads/default-profile.png'
